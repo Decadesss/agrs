9e62a0a5bbc44b37b05186d611a6fecbb0e54473
Decades <454121190@qq.com>
2023-12-04 20:44:39 +0800

    feat:设计检测-d的测试方法

--- src/test/java/org/example/ArgsParserTest.java
+++ src/test/java/org/example/ArgsParserTest.java
@@ -57,4 +57,28 @@ public class ArgsParserTest {
         assertEquals(logFlag.getValue(), "true");
         assertEquals(portFlag.getValue(), "8080");
     }
+
+    @Test
+    public void given_p_and_l_and_d_when_parse_return_port_and_logging(){
+        //Arrange
+        String args = "-l -p 8080 -d /usr/logs";
+
+        //Act
+        Schema schema = ArgsParser.parse(args);
+
+        //Assert
+        assertNotNull(schema);
+
+        Flag logFlag = schema.getFlagByName("l");
+        assertNotNull(logFlag);
+        Flag portFlag = schema.getFlagByName("p");
+        assertNotNull(portFlag);
+        Flag directoryFlag = schema.getFlagByName("d");
+        assertNotNull(directoryFlag);
+
+        assertEquals(logFlag.getValue(), "true");
+        assertEquals(portFlag.getValue(), "8080");
+        assertEquals(directoryFlag.getValue(), "/usr/logs");
+
+    }
 }
