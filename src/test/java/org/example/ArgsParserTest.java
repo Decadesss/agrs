621de67c4679bd228164063d19c775add357bb17
liyq <liyq@xmgis.cn>
2023-12-04 15:56:41 +0800

    feat:写出一个报错的测试方法

--- src/test/java/org/example/ArgsParserTest.java
+++ src/test/java/org/example/ArgsParserTest.java
@@ -37,4 +37,22 @@ public class ArgsParserTest {
         assertEquals(parse.getName(), "logging");
         assertEquals(parse.getValue(), "false");
     }
+
+    @Test
+    public void given_l_and_p_when_parse_return_logging_is_false(){
+        //Arrange
+        String args = "-l -p 8080";
+
+        //Act
+        Schema parse = ArgsParser.parse(args);
+
+        //Assert
+        assertNotNull(parse);
+        assertEquals(parse.getFlag(), "l");
+        assertEquals(parse.getName(), "logging");
+        assertEquals(parse.getValue(), "ture");
+        assertEquals(parse.getFlag(), "p");
+        assertEquals(parse.getName(), "port");
+        assertEquals(parse.getValue(), "8080");
+    }
 }
